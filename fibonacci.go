306eee03ece85886dbbcb6038305f57f73cb9cfe
Jeff Chao <jeffchao@me.com>
2014-03-24 11:25:29 -0700

    Add fibonacci skeleton. wip.

--- fibonacci.go
+++ fibonacci.go
@@ -1,1 +1,20 @@
 package backoff
+
+import (
+	"time"
+)
+
+type FibonacciBackoff struct {
+	Retries    int
+	MaxRetries int
+	Delay      time.Duration
+	Interval   time.Duration
+}
+
+func Fibonacci() *FibonacciBackoff {
+	return &FibonacciBackoff{
+		Retries:    0,
+		MaxRetries: 5,
+		Delay:      time.Duration(0),
+  }
+}
